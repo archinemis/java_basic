47399b360717361a7c10a99dfc69e22d9e8430a0
archinemis <denandra1628@gmail.com>
2019-02-08 21:30:50 +0700

    ketiga

--- src/K_if/Main.java
+++ src/K_if/Main.java
@@ -12,20 +12,10 @@ package K_if;
 public class Main {
     public static void main(String[] args) {
         
-        int a = 3;
+        boolean tanya = true;
         
-        switch (a) {
-            case 1:
-                System.out.println("nilai a = 1");
-                break;
-            case 2:
-                System.out.println("nilai a = 2");
-                break;
-            case 3:
-                System.out.println("nilai a = 3");
-                break;
-            default :
-                System.out.println("nilai a tidak diketahui");
+        if (tanya = true) {
+            System.out.println("Benar");
         }
         
     }
---
+++ src/M_switch/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package M_switch;
+
+/**
+ *
+ * @author Denandra
+ */
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        
+        switch (a) {
+            case 1:
+                System.out.println("nilai a = 1");
+                break;
+            case 2:
+                System.out.println("nilai a = 2");
+                break;
+            case 3:
+                System.out.println("nilai a = 3");
+                break;
+            default :
+                System.out.println("nilai a tidak diketahui");
+        }
+    }
+}
